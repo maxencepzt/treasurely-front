27e58ae9a4a07b228619f038170885077b8f5573
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-10-20 15:07:43 +0200

    feat: wrap App component with Redux store provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Router } from './components';
+import { Provider as StoreProvider } from "react-redux";
+import store from "./store";
 
 function App() {
 
   return (
-    <div className="flex justify-center items-center h-screen m-0 bg-gray-50">
-      <header></header>
-      <main className="w-full max-w-md px-4">
-        <Router />
-      </main>
-      <footer></footer>
-    </div>
+    <StoreProvider store={store}>
+      <div className="flex justify-center items-center h-screen m-0 bg-gray-50">
+        <header></header>
+        <main className="w-full max-w-md px-4">
+          <Router />
+        </main>
+        <footer></footer>
+      </div>
+    </StoreProvider>
   )
 }
 
