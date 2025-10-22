49819c5565275dbd943c9873659d2791675dd810
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-10-22 18:37:38 +0200

    feat: update UserProfilePicture component to display user profile image

--- src/components/UserProfilePicture.tsx
+++ src/components/UserProfilePicture.tsx
@@ -3,7 +3,7 @@ import {type user} from "../types/api";
 function UserProfilePicture({userData}:{userData?: user}) {
   return (
     <div className="flex justify-center">
-      <img src={userData ? "avatar" : "/user_profile_picture_default.png"} alt="Photo de profil"
+      <img src={userData ? "http://localhost:8000" + userData.profilePicture : "/user_profile_picture_default.png"} alt="Photo de profil"
            className="w-24 h-24 rounded-full border-4 border-white shadow-md"/>
     </div>
   )
