0706c1e64cc5d74f8a5afdb11857dd4ab7b57691
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-10-22 18:43:46 +0200

    feat: add LogoutButton and UserProfilePicture components to index export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,15 @@
 import Loading from './Loading';
 import LoginForm from './LoginForm';
+import LogoutButton from './LogoutButton.tsx';
 import Router from './Router';
 import UploadForm from './UploadForm';
+import UserProfilePicture from './UserProfilePicture.tsx';
 
 export {
   Loading,
   LoginForm,
+  LogoutButton,
+  Router, 
   UploadForm,
-  Router
+  UserProfilePicture,
 };
