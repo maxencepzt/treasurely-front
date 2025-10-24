44428c5424a90e45599c78fdc99d7449bde32e9c
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-10-24 11:02:43 +0200

    feat: integrate user context and add conditional LogoutButton in Index component

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -2,10 +2,15 @@ import FeaturesSection from '../components/landing/FeatureSection.tsx';
 import FooterLanding from '../components/landing/FooterLanding.tsx';
 import HeroSection from '../components/landing/HeroSection.tsx';
 import MiddleCTA from '../components/landing/MiddleCTA.tsx';
+import { useUser } from '../contexts/user/provider';
+import { LogoutButton } from '../components';
 
 function Index() {
+  const { user } = useUser();
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-green-50 to-emerald-100">
+      {(user && (<LogoutButton/>))}
       <HeroSection />
       <FeaturesSection />
       <MiddleCTA />
