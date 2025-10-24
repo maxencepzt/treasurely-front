14f0f1799814e0a7ff46936dce6c22fc997a08d6
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-10-24 16:05:27 +0200

    feat: implement SSO logout functionality in LogoutButton component

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -3,13 +3,28 @@ import { useNavigate } from 'react-router';
 
 import { logout } from '../store/slices/authSlice';
 import { useLogoutMutation } from '../store/slices/api';
+import { API_CONFIG } from '../config/api';
 
 function LogoutButton() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [logoutPost] = useLogoutMutation();
+
+    const logoutSSO = () => {
+      // TODO : fixer la requête
+        fetch(API_CONFIG.baseUrl + '/sso/logout/api', {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }).catch((error) => {
+          console.error('Error during SSO logout:', error);
+        });
+    }
     
     const handleLogout = () => {
+      logoutSSO();
       logoutPost({ refresh_token: localStorage.getItem('refresh_token') as string });
       dispatch(logout());
       navigate(0);
