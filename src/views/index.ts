fc943ae4e4dd4a734d4cb86984c36d954a11ff0d
maxencepzt <maxence.poizat@gmail.com>
2025-11-06 17:16:55 +0100

    feat: remove NotFound import from index.ts

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,11 +1,9 @@
 import Index from './Index.tsx';
 import Login from './Login.tsx';
-import NotFound from './NotFound.tsx';
 import UploadImage from './UploadImage.tsx';
 
 export {
   Index,
   Login,
-  NotFound,
   UploadImage,
 };
