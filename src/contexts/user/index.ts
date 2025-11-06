53ec9bcb169591266131027f8c0f9ac34ebc6fb9
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-11-06 20:38:36 +0100

    feat: update UserContext to apply React 19 recommendations

--- src/contexts/user/index.ts
+++ src/contexts/user/index.ts
@@ -1,14 +1,15 @@
+import { createContext, use } from 'react';
+
 import type { User } from '../../types/api';
-import { createContext, useContext } from 'react';
 
 type UserContextType = {
   user: User | null;
 }
 
-export const UserContext = createContext<UserContextType>({ user: null });
+export const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export function useUser(): UserContextType {
-  const context = useContext(UserContext);
+  const context = use(UserContext);
   if (context === undefined) {
     throw new Error('useUser must be used within a UserProvider');
   }
