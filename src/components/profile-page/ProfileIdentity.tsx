b75867c5c593b5177afa6bcd5b2cfad420222812
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-11-14 00:28:54 +0100

    feat: enhance ProfileIdentity component with editable state and layout adjustments

--- src/components/profile-page/ProfileIdentity.tsx
+++ src/components/profile-page/ProfileIdentity.tsx
@@ -1,15 +1,23 @@
+import { faGear } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import type { User } from '../../types/api.ts';
 import UserProfilePicture from '../UserProfilePicture.tsx';
 
-export function ProfileIdentity({ user }: { user: User }) {
+export function ProfileIdentity({ user, editable = false }: { user: User, editable?: boolean }) {
   return (
-    <div className="flex items-center gap-4">
-      {/* Avatar */}
-      <UserProfilePicture userData={user} />
+    <div className="flex items-center justify-between p-3">
+      {/* Avatar à gauche */}
+      <UserProfilePicture userData={user} size={52} />
 
-      {/* Nickname */}
-      <div className="flex flex-col">
-        <span className="text-xl font-semibold">{user.nickname}</span>
+      {/* Nickname au centre */}
+      <span className="text-xl font-semibold">{user.nickname}</span>
+
+      {/* Icon d'édition à droite */}
+      <div className="w-10">
+        {editable ? (
+          <FontAwesomeIcon icon={faGear} className="text-3xl" />
+        ) : null}
       </div>
     </div>
   );
