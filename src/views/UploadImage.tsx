bdd76a23b3132fe4a057476f9f3a2e764a3f15e9
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-11-17 17:00:26 +0100

    feat: update image upload logic to include user ID and handle user context

--- src/views/UploadImage.tsx
+++ src/views/UploadImage.tsx
@@ -1,15 +1,19 @@
 import type { FormEvent } from 'react';
 
 import { Loading, UploadForm } from '../components';
-import { useUploadImageMutation, useUserProfilePictureDeleteMutation } from '../store/slices/api';
+import { useUserUploadProfilePictureMutation, useUserProfilePictureDeleteMutation } from '../store/slices/api';
+import { useUser } from '../contexts/user';
 
 function UploadImage() {
   const [userProfilePictureDelete, { isLoading: isLoadingDelete, error: errorDelete }] = useUserProfilePictureDeleteMutation();
-  const [uploadImagePost, { isLoading: isLoadingUpload, error: errorUpload }] = useUploadImageMutation();
+  const [uploadImagePost, { isLoading: isLoadingUpload, error: errorUpload }] = useUserUploadProfilePictureMutation();
+  const { user } = useUser();
 
   async function handleSubmit() {
+    if (!user) return;
+
     try {
-      await userProfilePictureDelete(null).unwrap();
+      await userProfilePictureDelete(user.id).unwrap();
 
       window.location.reload();
     } catch (error) {
@@ -18,10 +22,12 @@ function UploadImage() {
   }
 
   async function handleChange(e: FormEvent<HTMLInputElement>) {
+    if (!user) return;
+
     const formData = new FormData(e.currentTarget.form!);
 
     try {
-      await uploadImagePost(formData).unwrap();
+      await uploadImagePost({userId: user.id, formData }).unwrap();
 
       window.location.reload();
     } catch (error) {
