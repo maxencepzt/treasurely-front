15cd79e24fb752cd0f1aa9cf20465d46fcfa9978
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-11-17 16:07:39 +0100

    feat: add treasureHuntGetById query to API for fetching treasure hunt details by ID

--- src/store/slices/api.ts
+++ src/store/slices/api.ts
@@ -3,7 +3,7 @@ import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolk
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { API_CONFIG } from '../../config/api';
-import type { User } from "../../types/api";
+import type { TreasureHuntAPI, User } from "../../types/api";
 import type { rootState } from '../index';
 import { logout, setCredentials } from './authSlice';
 
@@ -109,6 +109,12 @@ const api = createApi({
         method: 'DELETE',
       }),
     }),
+    treasureHuntGetById: build.query<TreasureHuntAPI, { id: number }>({
+      query: ({ id }) => ({
+        url: `treasure_hunts/${id}`,
+        method: 'GET',
+      }),
+    }),
   }),
 });
 
@@ -137,5 +143,6 @@ export const {
   useLazyGetAuthentifiedUserQuery,
   useLogoutMutation,
   useLogoutSSOMutation,
+  useTreasureHuntGetByIdQuery,
 } = api;
 export default api;
