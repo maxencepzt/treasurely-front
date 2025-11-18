571e2980f1783e43a7b9d42007ff3971362f8668
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-11-18 11:20:58 +0100

    feat: add getIdFromUrl function to extract team ID from URL

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -20,3 +20,14 @@ export const getErrorMessage = (error?: FetchBaseQueryError | SerializedError) =
   // SerializedError
   return error.message || 'Une erreur est survenue';
 };
+
+/**
+ * Récupère l'ID d'une équipe à partir de son URL
+ * @param url L'URL de l'équipe (ex: "/teams/1")
+ * @example getIdFromUrl("/teams/1") // retourne 1
+ */
+export function getIdFromUrl(url: string): number {
+  const matches = url.match(/(\d+)/g);
+  if (!matches) throw new Error(`${url} is not a valid URL`);
+  return Number(matches[matches.length - 1]);
+}
