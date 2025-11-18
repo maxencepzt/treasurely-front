33b5915417e1ea558c832acf1f44419bb2c51438
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-11-18 09:57:08 +0100

    feat: enhance TreasureHunt component to handle errors and display appropriate messages

--- src/views/treasure-hunt/TreasureHunt.tsx
+++ src/views/treasure-hunt/TreasureHunt.tsx
@@ -1,4 +1,4 @@
-import {useParams} from "react-router";
+import { useParams } from "react-router";
 
 import {Loading} from "../../components";
 import { useTreasureHuntGetByIdQuery } from "../../store/slices/api.ts";
@@ -11,9 +11,26 @@ export default function TreasureHunt() {
   if (!params.id) {
     throw new Error('No id provided.');
   }
-  const { data: treasureHunt, isLoading } = useTreasureHuntGetByIdQuery({id: parseInt(params.id)});
+  const { data: treasureHunt, isLoading, error } = useTreasureHuntGetByIdQuery({id: parseInt(params.id)});
 
   if (isLoading) return <Loading />;
+
+  if (error) {
+    let status = 500;
+    let message = "Une erreur est survenue";
+
+    if ("status" in error) {
+      status = typeof error.status === "number" ? error.status : 500;
+      message = (error.data && typeof error.data === "object" && "message" in error.data)
+        ? (error.data as any).message
+        : "Erreur serveur";
+    } else if ("message" in error) {
+      message = error.message || message;
+    }
+
+    return <ErrorView status={status} message={message} />;
+  }
+
   if (!treasureHunt) return <ErrorView status={404} message="Chasse introuvable" />;
   if (!treasureHunt.public) return <TreasureHuntPrivate />;
 
