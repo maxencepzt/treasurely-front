bafffe6ea610d9fa770f9f33058b518bf996ed00
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-11-19 09:59:26 +0100

    feat: update ProfileStatCard and ProfileStatSection for improved user stats display

--- src/components/profile-page/ProfileStatCard.tsx
+++ src/components/profile-page/ProfileStatCard.tsx
@@ -1,6 +1,6 @@
 export default function ProfileStatCard({ title, emoji, value }: { title: string, emoji: string, value: number | string }) {
   return (
-    <div className="bg-white rounded-2xl px-10 py-6 shadow-lg text-center border-2 border-green-700 h-26 flex flex-col justify-center">
+    <div className="bg-white rounded-2xl px-10 py-6 shadow-lg text-center border-3 border-green-700 h-30 flex flex-col justify-center w-48">
       <h3 className="text-base font-bold text-gray-900 mb-1 text-center">
         {title}
       </h3>
--- src/components/profile-page/ProfileStatSection.tsx
+++ src/components/profile-page/ProfileStatSection.tsx
@@ -3,12 +3,16 @@ import formatDuration from '../../utils/formatDuration.ts';
 import ProfileStatCard from './ProfileStatCard.tsx';
 
 export default function ProfileStatSection({ user }: { user: User }) {
+  // TODO: Number of enigmes should come from the API
+  // TODO: Number of score should come from the API
   return (
     <>
       <span className="pl-4">Stats</span>
-      <div className="flex flex-wrap justify-between p-4">
+      <div className="flex flex-wrap justify-between p-4 gap-y-6">
         <ProfileStatCard title="Chasses" emoji="💎" value={user.totalHunt} />
-        <ProfileStatCard title="Enigmes" emoji="🧩" value={formatDuration(user.totalTime)} />
+        <ProfileStatCard title="Temps de jeu" emoji="⏲️" value={formatDuration(user.totalTime)} />
+        <ProfileStatCard title="Enigmes" emoji="🧩" value={420} />
+        <ProfileStatCard title="Score" emoji="🪙" value={8400} />
       </div>
     </>
   );
