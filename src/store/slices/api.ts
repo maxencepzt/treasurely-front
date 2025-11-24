511df54dfbdbe26601d57e4e941b1a03929e59bc
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-11-24 11:53:16 +0100

    fix: correct import statement formatting in api.ts

--- src/store/slices/api.ts
+++ src/store/slices/api.ts
@@ -2,7 +2,7 @@ import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolk
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { API_CONFIG } from '../../config/api';
-import type { TeamAPI, TeamMembersAPI,TeamTreasureHuntsAPI, TreasureHuntAPI, User, UserTeamsAPI } from "../../types/api";
+import type { TeamAPI, TeamMembersAPI, TeamTreasureHuntsAPI, TreasureHuntAPI, User, UserTeamsAPI } from "../../types/api";
 import type { rootState } from '../index';
 import { logout, setCredentials } from './authSlice';
 
