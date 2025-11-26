31ebfefe3a27582539317258ebb8e03c91479acc
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-11-26 09:48:32 +0100

    feat: add Dashboard component and route for connected users

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route,Routes } from "react-router";
 
-import { Index, Login, UserUploadProfilePicture } from '../views';
+import { Dashboard, Index, Login, UserUploadProfilePicture } from '../views';
 import ErrorView from '../views/Error.tsx';
 import ErrorRouteWrapper from '../views/ErrorRouteWrapper.tsx';
 import Profile from '../views/profile/Profile.tsx';
@@ -14,6 +14,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Index/>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/upload" element={<UserUploadProfilePicture/>} />
         <Route path="/profile" element={<Profile/>} />
         <Route path="/profile/:id" element={<Profile/>} />
