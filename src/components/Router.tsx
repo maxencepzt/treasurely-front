67491bf1c02fccd83c11b4408ca8d5d630bc1285
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-11-26 23:46:50 +0100

    feat: refactor Index component to conditionally render Dashboard or LandingPage based on user authentication

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route,Routes } from "react-router";
 
-import { Dashboard, Index, Login, UserUploadProfilePicture } from '../views';
+import { Index, Login, UserUploadProfilePicture } from '../views';
 import ErrorView from '../views/error/Error.tsx';
 import ErrorRouteWrapper from '../views/error/ErrorRouteWrapper.tsx';
 import Profile from '../views/profile/Profile.tsx';
@@ -14,7 +14,6 @@ function Router() {
       <Routes>
         <Route path="/" element={<Index/>} />
         <Route path="/login" element={<Login/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/upload" element={<UserUploadProfilePicture/>} />
         <Route path="/profile" element={<Profile/>} />
         <Route path="/profile/:id" element={<Profile/>} />
--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -1,38 +1,11 @@
-import { useNavigate } from 'react-router';
-
-import { LogoutButton } from '../components';
-import FeaturesSection from '../components/landing/FeatureSection.tsx';
-import FooterLanding from '../components/landing/FooterLanding.tsx';
-import HeroSection from '../components/landing/HeroSection.tsx';
-import MiddleCTA from '../components/landing/MiddleCTA.tsx';
-import LoginSSOButton from '../components/LoginSSOButton';
 import { useUser } from '../contexts/user';
+import Dashboard from './dashboard/Dashboard';
+import LandingPage from './landing-page/LandingPage';
 
 function Index() {
   const { user } = useUser();
-  const navigate = useNavigate();
 
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-green-50 to-emerald-100">
-      {(user && (
-        <div className="fixed top-4 right-4 z-50 flex gap-2">
-          <button
-            type="button"
-            onClick={() => navigate('/dashboard')}
-            className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition-colors"
-          >
-            Mon Tableau de Bord
-          </button>
-          <LogoutButton/>
-          <LoginSSOButton/>
-        </div>
-      ))}
-      <HeroSection />
-      <FeaturesSection />
-      <MiddleCTA />
-      <FooterLanding />
-    </div>
-  );
+  return user ? <Dashboard /> : <LandingPage />;
 }
 
 export default Index;
