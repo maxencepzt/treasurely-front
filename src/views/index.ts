eeb6d0ff742e1f9329e8f6effa8f4dde29f3dc57
Ylan Nicolas <ylan.nicolas@etudiant.univ-reims.fr>
2025-11-26 16:00:18 +0100

    feat: add Riddle component to index export for riddle functionality

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,9 +1,11 @@
 import Index from './Index.tsx';
 import Login from './Login.tsx';
+import Riddle from './riddle/Riddle.tsx';
 import UserUploadProfilePicture from './UserUploadProfilePicture.tsx';
 
 export {
   Index,
   Login,
+  Riddle,
   UserUploadProfilePicture,
 };
