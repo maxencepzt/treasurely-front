b63715a26c52a53984ed0c381bb1a49a56ea400f
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-11-26 22:26:42 +0100

    feat: improve error parsing in parseApiError function

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -14,18 +14,22 @@ export function getIdFromUrl(url: string): number {
  * @param error L'erreur à parser
  * @example parseApiError(error) // retourne { status: 404, message: "Not Found" }
  */
-export function parseApiError(error: any): { status: number, message: string } {
+export function parseApiError(error: unknown): { status: number, message: string } {
   let status = 500;
   let message = "An error occurred";
 
-  if (error) {
+  if (error && typeof error === "object") {
     if ("status" in error) {
       status = typeof error.status === "number" ? error.status : 500;
-      message = (error.data && typeof error.data === "object" && "message" in error.data)
-        ? error.data.message
-        : "Server error";
+
+      if ("data" in error && error.data && typeof error.data === "object" && "message" in error.data) {
+        const data = error.data as { message: unknown };
+        message = typeof data.message === "string" ? data.message : "Server error";
+      } else {
+        message = "Server error";
+      }
     } else if ("message" in error) {
-      message = error.message || message;
+      message = typeof error.message === "string" ? error.message : message;
     }
   }
 
